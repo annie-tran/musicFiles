54526b243b45ba6c4099fdc6a7ecbe5ac9956ba9
Annie Tran <annietran@dhcp-rhodes-262.redrover.cornell.edu>
2018-04-16 01:33:04 -0400

    fit

--- beats.py
+++ beats.py
@@ -52,13 +52,13 @@ def pyaudio_callback(_in_data, _frame_count, _time_info, _status):
     return (audiobuf, pyaudio.paContinue)
 
 # create pyaudio stream with frames_per_buffer=hop_s and format=paFloat32
-p = pyaudio.PyAudio()
+# p = pyaudio.PyAudio()
 pyaudio_format = pyaudio.paFloat32
 frames_per_buffer = hop_s
 n_channels = 1
-stream = p.open(format=pyaudio_format, channels=n_channels, rate=samplerate,
-        output=True, frames_per_buffer=frames_per_buffer,
-        stream_callback=pyaudio_callback)
+# stream = p.open(format=pyaudio_format, channels=n_channels, rate=samplerate,
+#         output=True, frames_per_buffer=frames_per_buffer,
+#         stream_callback=pyaudio_callback)
 
 # # start pyaudio stream
 # stream.start_stream()
