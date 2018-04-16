9b54326047d5657055513655435a1a9d49b3bae2
Annie Tran <annietran@dhcp-rhodes-262.redrover.cornell.edu>
2018-04-16 00:53:31 -0400

    fix

--- beats.py
+++ beats.py
@@ -39,36 +39,36 @@ a_tempo = aubio.tempo("default", win_s, hop_s, samplerate)
 # create a simple click sound
 click = 0.7 * np.sin(2. * np.pi * np.arange(hop_s) / hop_s * samplerate / 3000.)
 
-# pyaudio callback
-def pyaudio_callback(_in_data, _frame_count, _time_info, _status):
-    samples, read = a_source()
-    is_beat = a_tempo(samples)
-    if is_beat:
-        samples += click
-        print ('tick') # avoid print in audio callback
-    audiobuf = samples.tobytes()
-    if read < hop_s:
-        return (audiobuf, pyaudio.paComplete)
-    return (audiobuf, pyaudio.paContinue)
+# # pyaudio callback
+# def pyaudio_callback(_in_data, _frame_count, _time_info, _status):
+#     samples, read = a_source()
+#     is_beat = a_tempo(samples)
+#     if is_beat:
+#         samples += click
+#         print ('tick') # avoid print in audio callback
+#     audiobuf = samples.tobytes()
+#     if read < hop_s:
+#         return (audiobuf, pyaudio.paComplete)
+#     return (audiobuf, pyaudio.paContinue)
 
-# create pyaudio stream with frames_per_buffer=hop_s and format=paFloat32
-p = pyaudio.PyAudio()
-pyaudio_format = pyaudio.paFloat32
-frames_per_buffer = hop_s
-n_channels = 1
-stream = p.open(format=pyaudio_format, channels=n_channels, rate=samplerate,
-        output=True, frames_per_buffer=frames_per_buffer,
-        stream_callback=pyaudio_callback)
+# # create pyaudio stream with frames_per_buffer=hop_s and format=paFloat32
+# p = pyaudio.PyAudio()
+# pyaudio_format = pyaudio.paFloat32
+# frames_per_buffer = hop_s
+# n_channels = 1
+# stream = p.open(format=pyaudio_format, channels=n_channels, rate=samplerate,
+#         output=True, frames_per_buffer=frames_per_buffer,
+#         stream_callback=pyaudio_callback)
 
-# start pyaudio stream
-stream.start_stream()
+# # start pyaudio stream
+# stream.start_stream()
 
-# wait for stream to finish
-while stream.is_active():
-    time.sleep(0.1)
+# # wait for stream to finish
+# while stream.is_active():
+#     time.sleep(0.1)
 
-# stop pyaudio stream
-stream.stop_stream()
-stream.close()
-# close pyaudio
-p.terminate()
+# # stop pyaudio stream
+# stream.stop_stream()
+# stream.close()
+# # close pyaudio
+# p.terminate()
